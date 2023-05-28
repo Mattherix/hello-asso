8ba8cb69b3be1de7a51eed2946d56446bd362110
mattherix <mattherix@protonmail.com>
2023-05-28 23:05:31 +0200

    docs: error documentation

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,4 @@
-//! `error` errors this crate can return
+//! Errors this crate can return
 
 use std::fmt::Display;
 
@@ -6,15 +6,26 @@ use thiserror::Error;
 
 use serde::Deserialize;
 
+/// Errors that may occur when using the [client](crate::HelloAsso)
+///
+/// It can ether be a [Reqwest Error](reqwest::Error) or an [Authentication Error](crate::AuthenticationError)
+// TODO: Decode error
 #[derive(Error, Debug)]
 #[non_exhaustive]
 pub enum Error {
     #[error("request failed")]
     ReqwestErr(#[from] reqwest::Error),
-    #[error("authentifaication failed")]
+    #[error("authentification failed")]
     AuthErr(AuthenticationError),
 }
 
+/// Authentication Error that may occur when trying to access the api
+///
+/// `helloasso` will raise an [AuthenticationError](crate::AuthenticationError) {
+///     error: "unauthorized_client",
+///     error_description: "Invalid client_id '{client_id}'"
+/// } even when it is your `client_secret` witch is wrong.
+/// This behaviour is from the web api.
 #[derive(Error, Debug, Deserialize)]
 pub struct AuthenticationError {
     pub error: String,
