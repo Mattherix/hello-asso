b13daa8af1c50639c521ff8fd1d0c0012cb161c7
mattherix <mattherix@protonmail.com>
2023-05-28 21:54:02 +0200

    feat(error): impl Error using thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,16 +1,28 @@
 //! `error` errors this crate can return
 
+use std::fmt::Display;
+
+use thiserror::Error;
+
 use serde::Deserialize;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 #[non_exhaustive]
 pub enum Error {
-    ReqwestErr(reqwest::Error),
+    #[error("request failed")]
+    ReqwestErr(#[from] reqwest::Error),
+    #[error("authentifaication failed")]
     AuthErr(AuthenticationError),
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Error, Debug, Deserialize)]
 pub struct AuthenticationError {
     pub error: String,
     pub error_description: String,
+}
+
+impl Display for AuthenticationError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}: {}", self.error, self.error_description)
+    }
 }
